0a5e7537099386654dd7957a925e2b39c87627b7
tom_glb <ASUS@LAPTOP-J89KJ169>
2020-03-19 09:00:28 +0800

    maven

---
+++ MakeFriends/src/main/java/com/atguigu/maven/MakeFriends.java
@@ -0,0 +1,13 @@
+package com.atguigu.maven;
+
+public class MakeFriends {
+	
+	public String makeFriends(String name){
+		HelloFriend friend = new HelloFriend();
+		friend.sayHelloToFriend("litingwei");
+		String str = "Hey,"+friend.getMyName()+" make a friend please.";
+		System.out.println(str);
+		return str;
+	}
+
+}
---
+++ MakeFriends/src/test/java/com/atguigu/maven/MakeFriendsTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.maven;
+
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
+
+public class MakeFriendsTest {
+	@Test
+	public void testMakeFriends() {
+		MakeFriends makeFriend = new MakeFriends();
+		String str = makeFriend.makeFriends("litingwei");
+		assertEquals("Hey,John make a friend please.", str);
+	}
+}
